13e0ec6316ba2527212ebb9ab0780a33ea771efd
John-Feil <johnathancfeil@gmail.com>
2022-04-22 16:19:29 -0400

    Finished

---
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Bank.java
@@ -0,0 +1,36 @@
+package rocks.zipcode.quiz4.objectorientation.account;
+import java.util.ArrayList;
+
+/**
+ * @author leon on 27/12/2018.
+ */
+public class Bank {
+    ArrayList<BankAccount> bankAccountArrayList;
+
+    public Bank(){
+        bankAccountArrayList = new ArrayList<>();
+    }
+
+    public void removeBankAccountByIndex(Integer indexNumber) {
+        bankAccountArrayList.remove((int)indexNumber);
+    }
+
+    public void addBankAccount(BankAccount bankAccount) {
+        bankAccountArrayList.add(bankAccount);
+    }
+
+    public Boolean containsBankAccount(BankAccount bankAccount) {
+        return bankAccountArrayList.contains(bankAccount);
+    }
+
+    public ArrayList<BankAccount> getBankAccountArrayList() {
+        return bankAccountArrayList;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "bankAccountArrayList=" + bankAccountArrayList +
+                '}';
+    }
+}
---
+++ src/main/java/rocks/zipcode/quiz4/objectorientation/account/Employee.java
@@ -0,0 +1,75 @@
+package rocks.zipcode.quiz4.objectorientation.account;
+
+/**
+ * @author leon on 30/12/2018.
+ */
+public class Employee implements Transactable, Worker{
+    private BankAccount bankAccount;
+    private Double hoursWorked;
+    private Double hourlyWage;
+    private Double moneyEarned;
+
+    public Employee() {
+        this.bankAccount = new BankAccount();
+        this.hourlyWage = 35.0;
+        this.hoursWorked = 0.0;
+        this.moneyEarned = 0.0;
+    }
+
+    public Employee(BankAccount bankAccount, Double hoursWorked, Double hourlyWage, Double moneyEarned) {
+        this.bankAccount = bankAccount;
+        this.hoursWorked = hoursWorked;
+        this.hourlyWage = hourlyWage;
+        this.moneyEarned = moneyEarned;
+    }
+
+    public Employee(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+        this.hourlyWage = 35.0;
+        this.hoursWorked = 0.0;
+        this.moneyEarned = 0.0;
+    }
+
+    @Override
+    public void increaseHoursWorked(Double numberOfHours) {
+        this.hoursWorked += numberOfHours;
+    }
+
+    @Override
+    public Double getHoursWorked() {
+        return this.hoursWorked;
+    }
+
+    @Override
+    public Double getHourlyWage() {
+        return this.hourlyWage;
+    }
+
+    @Override
+    public Double getMoneyEarned() {
+        return this.moneyEarned;
+    }
+
+    public BankAccount getBankAccount() {
+        return this.bankAccount;
+    }
+
+    public void setBankAccount(BankAccount bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    @Override
+    public void deposit(Double amountToIncreaseBy) {
+        bankAccount.deposit(amountToIncreaseBy);
+    }
+
+    @Override
+    public void withdrawal(Double amountToDecreaseBy) {
+        bankAccount.withdrawal(amountToDecreaseBy);
+    }
+
+    @Override
+    public Double getBalance() {
+        return bankAccount.getBalance();
+    }
+}
